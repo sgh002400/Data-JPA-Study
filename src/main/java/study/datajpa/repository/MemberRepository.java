0740b08bc8dc2c127e24cdcfedb2cd1611fddd0f
Jihwan <sgh002400@naver.com>
2021-08-29 16:24:47 +0900

    반환 타입

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -8,6 +8,7 @@ import study.datajpa.entity.Member;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -46,4 +47,19 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username in :names") //Collection 타입으로 in절 지원
     List<Member> findByNames(@Param("names") Collection<String> names);
+
+    /** 유연한 반환타입 지원
+     * find 뒤에 붙는 List, Member, Optional은 의미 있는게 아님! 아무거나 적어도 상관 없다.
+     */
+    List<Member> findListByUsername(String name); //컬렉션 - 결과 없음: 빈 컬렉션 반환
+    Member findMemberByUsername(String name); //단건 - 결과 없음: null 반환, 결과가 2건 이상: javax.persistence.NonUniqueResultException 예외 발생
+    Optional<Member> findOptionalByUsername(String name); //단건 Optional
+
+    /**
+     *  참고: 단건으로 지정한 메서드를 호출하면 스프링 데이터 JPA는 내부에서 JPQL의
+     * Query.getSingleResult() 메서드를 호출한다. 이 메서드를 호출했을 때 조회 결과가 없으면
+     * javax.persistence.NoResultException 예외가 발생하는데 개발자 입장에서 다루기가 상당히
+     * 불편하다. 스프링 데이터 JPA는 단건을 조회할 때 이 예외가 발생하면 예외를 무시하고 대신에 null 을
+     * 반환한다.
+     */
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -12,6 +12,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -126,5 +127,17 @@ public class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> findMemberList = memberRepository.findListByUsername("AAA");
+        Member findMember = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> findOptional = memberRepository.findOptionalByUsername("AAA");
     }
 }
